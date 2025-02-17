c83f49cf842a6ae4b1858b4985a207ca3035f360
baisali609 <baisali609@gmail.com>
2025-02-17 09:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.getElementById('submit').addEventListener('click', function() {
     const isbn = document.getElementById('isbn').value;
 
 	const list = document.getElementById('book-list');
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${title}</td>
-                <td>${author}</td>
-                <td>${isbn}</td>
-                <td><button class="btn btn-danger delete">Clear</button></td>
-            `;
+    const row = document.createElement('tr');
+    // row.innerHTML = `
+    //             <td>${title}</td>
+    //             <td>${author}</td>
+    //             <td>${isbn}</td>
+    //             <td><button class="btn btn-danger delete">Clear</button></td>
+    //         `;
             list.appendChild(row);
 
             document.getElementById('title').value = '';
