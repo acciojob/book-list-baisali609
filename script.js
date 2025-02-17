1ed6467624e88ad34658c5dbb1c4f677a3feb678
baisali609 <baisali609@gmail.com>
2025-02-17 08:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener('click', function() {
+	const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+	const list = document.getElementById('book-list');
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${title}</td>
+                <td>${author}</td>
+                <td>${isbn}</td>
+                <td><button class="btn btn-danger delete">Clear</button></td>
+            `;
+            list.appendChild(row);
+
+            document.getElementById('title').value = '';
+            document.getElementById('author').value = '';
+            document.getElementById('isbn').value = '';
+        });
+
+        document.getElementById('book-list').addEventListener('click', function(e) {
+            if (e.target.classList.contains('delete')) {
+                e.target.parentElement.parentElement.remove();
+            }
+        });
